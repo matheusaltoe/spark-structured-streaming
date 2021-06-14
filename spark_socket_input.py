8f949d7a2fe9c9726669b03670d1f63543557044
Matheus Altoe <matheus.altoe@nappsolutions.com>
2021-06-14 18:02:24 -0300

    Init repo

---
+++ spark_socket_input.py
@@ -0,0 +1,38 @@
+#Import Libraries
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import *
+
+#Parameters to listen socket 
+host = "127.0.0.1"
+port = "9999"
+
+#Create Spark Session
+spark = SparkSession \
+        .builder \
+        .appName("firstSparkStreaming") \
+        .getOrCreate()
+
+#Create Streaming DataFrame by socket input source
+initDF = (spark
+  .readStream
+  .format("socket")
+  .option("host", host)
+  .option("port", port)
+  .load())
+
+#Dataframe printSchema
+wordCount = (initDF
+  .select(explode(split(col("value"), " ")).alias("words"))
+  .groupBy("words")
+  .count()
+  )
+wordCount.printSchema()
+
+#Show streaming DataFrame
+wordCount.writeStream \
+  .outputMode("complete") \
+  .option("truncate", False) \
+  .format("console") \
+  .start() \
+  .awaitTermination()
